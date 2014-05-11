4d0ea5e24bda60893fbfa559a53f590c7e54f7f8
Neil Anderson <neil.t.anderson@gmail.com>
2014-05-12 00:16:57 +0100

    add a timing test

--- regard-client.js
+++ regard-client.js
@@ -1,8 +1,25 @@
 define(function () {
 
+  var _timedEvents = [];
+  
   var _initialTime = Date.now();
+  var _timeEvent = function(eventName, funcToTime){
+    var begin = Date.now();
+    funcToTime();
+    var end = Date.now();
+    
+    _timedEvents.push({
+      name : eventName,
+      startTime : begin,
+      endTime : end,
+      duration: end - begin
+    });
+  };
+  
 
   return {
-    initialTime : _initialTime
+    initialTime : _initialTime,
+    timeEvent : _timeEvent,
+    timedEvents : _timedEvents
   }
 });
--- testing/regard-client-tests.js
+++ testing/regard-client-tests.js
@@ -6,9 +6,21 @@ requirejs.config({
 
 
 requirejs(["regardclient"], function(regard) {
-
+  
   test( "initial time is set", function() {
     ok( regard.initialTime > 0 , "initial time set to " + regard.initialTime );
+  });
+  
+  test( "timed event test", function() {
+    regard.timeEvent("timed event", function(){
+      for(var i =0; i < 1000; ++i){
+        console.log(i);
+      }
+    });
+    
+    var duration = regard.timedEvents[0].duration;
+    
+    ok( duration > 0, "recorded " + duration + " ms");
   });
   
 });
