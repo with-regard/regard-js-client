5c5caf6c76cb0647754aebbb22f35bc065c4852c
Neil Anderson <neil.t.anderson@gmail.com>
2014-05-19 22:24:30 +0100

    make tests run while gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var fs = require('fs');
 var gulp = require('gulp');
+var qunit = require('node-qunit-phantomjs');
+
 //var browserify = require('gulp-browserify');
 var browserify = require('browserify');
 
 gulp.task('bundle', function() {
-	browserify('./index.js')
-		.bundle({standalone: 'regardClient'})
+	browserify('./index.js').require('./index.js', {expose: 'regard'})
+		.bundle()
 		.pipe(fs.createWriteStream(__dirname + '/dist/regard.js'));
 });
 
-gulp.task('default', ['bundle']);
+gulp.task('test', function() {
+     qunit('./testing/run_tests.html');
+});
+
+gulp.task('default', ['bundle', 'test']);
