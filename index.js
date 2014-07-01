7bc66a1232474806a74888c5fffbf0cbece3f9c8
Neil Anderson <neil.t.anderson@gmail.com>
2014-07-01 22:13:48 +0100

    wrap a lot of internal stuff up further behind the interface

--- index.js
+++ index.js
@@ -1,116 +1,94 @@
-  var moment = require("moment");
-  var rsvp = require("rsvp");
+var moment = require('moment');
+var rsvp = require('rsvp');
 
-  var _createGuid = function () {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-          var r = Math.random() * 16 | 0,
-              v = c === 'x' ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
-      });
-  };
+var _createGuid = function () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0,
+      v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
 
-  var _createUndefinedError = function (functionName, identifier) {
-      return new Error(functionName + "(): " + identifier + " must be defined");
+var _initId = function (idName, storage) {
+  var id = storage.getItem(idName);
+
+  if (!id) {
+    id = _createGuid();
+    storage.setItem(idName, id);
   }
 
-  var _timedEvents = [];
-  var _events = [];
-  var _submitEventsImmediately = true;
-  var _regardURL = "";
-  var _sessionId = _createGuid();
-  var _userId = _createGuid();
+  return id;
+};
 
-  var _timedEvent = function (eventName, funcToTime) {
-      var begin = moment();
-      funcToTime();
-      var end = moment();
+var _sessionId = _initId('sessionId', sessionStorage);
+var _userId = _initId('userId', localStorage);
+var _product = '';
+var _organization = '';
 
-      _timedEvents.push({
-          name: eventName,
-          startTime: begin.valueOf(),
-          endTime: end.valueOf(),
-          duration: end.diff(begin)
-      });
+var _createUndefinedError = function (functionName, identifier) {
+  return new Error(functionName + '(): ' + identifier + ' must be defined');
+};
+
+var _getRegardUrl = function () {
+  if (!_organization) {
+    _createUndefinedError('_getRegardUrl', '_organization');
+  };
+  if (!_product) {
+    _createUndefinedError('_getRegardUrl', '_product');
   };
 
-  var _postEvent = function (event) {
-      return new rsvp.Promise(function (resolve, reject) {
-          if (!event) {
-              reject(_createUndefinedError("_postEvent", "event"));
-          }
+  return 'http://api.withregard.io/track/v1/' + _organization + '/' + _product + '/event';
+};
 
-          var postEventRequest = new XMLHttpRequest();
-          postEventRequest.onreadystatechange = function () {
-              if (postEventRequest.readyState === 4) {
-                  if (postEventRequest.status === 200) {
-                      resolve(event);
-                  } else {
-                      reject(new Error(postEventRequest.statusText));
-                  }
-              }
-          };
+var _postEvent = function (event) {
+  return new rsvp.Promise(function (resolve, reject) {
+    if (!event) {
+      reject(_createUndefinedError('_postEvent', 'event'));
+    }
+    var postEventRequest = new XMLHttpRequest();
+    postEventRequest.onreadystatechange = function () {
+      if (postEventRequest.readyState === 4) {
+        if (postEventRequest.status === 200) {
+          resolve(event);
+        } else {
+          reject(new Error(postEventRequest.statusText));
+        }
+      }
+    };
+    postEventRequest.open('POST', _getRegardUrl(), true);
+    postEventRequest.send(JSON.stringify(event));
+  });
+};
 
-          postEventRequest.open("POST", _regardURL, true);
-          postEventRequest.send(JSON.stringify(event));
-      });
-  };
+var _trackEvent = function (eventType, props) {
+  return new rsvp.Promise(function (resolve, reject) {
+    if (!eventType) {
+      reject(_createUndefinedError('_trackEvent', 'eventType'));
+    }
+    if (!_sessionId) {
+      reject(_createUndefinedError('_postEvent', 'A session Id'));
+    }
+    if (!_userId) {
+      reject(_createUndefinedError('_postEvent', 'A user Id'));
+    }
+    var event = {
+      'event-type': eventType,
+      'session-id': _sessionId,
+      'user-id': _userId,
+      'time': moment().valueOf()
+    };
+    if (props) {
+      event['data'] = props;
+    }
 
-  var _trackEvent = function (eventType, props) {
-      return new rsvp.Promise(function (resolve, reject) {
-          if (!eventType) {
-              reject(_createUndefinedError("_trackEvent", "eventType"));
-          }
-          if (!_sessionId) {
-              reject(_createUndefinedError("_postEvent", "A session Id"));
-          }
-          if (!_userId) {
-              reject(_createUndefinedError("_postEvent", "A user Id"));
-          }
+    _postEvent(event).then(resolve, reject);
+  });
+};
 
-          var event = {
-              "event-type": eventType,
-              "session-id": _sessionId,
-              "user-id": _userId,
-              "time": moment().valueOf()
-          };
-
-          if (props) {
-              event["data"] = props;
-          }
-
-          if (_submitEventsImmediately) {
-              _postEvent(event).then(resolve, reject);
-          } else {
-              _events.push(event);
-              resolve(event);
-          }
-      });
-  }
-
-  module.exports.trackTimedEvent = _timedEvent;
-  module.exports.timedEvents = _timedEvents;
-  module.exports.events = _events;
-  module.exports.trackEvent = _trackEvent;
-  module.exports.startNewSession = function () {
-      _sessionId = _createGuid();
-      return _sessionId;
-  };
-  module.exports.createNewUser = function () {
-      _userId = _createGuid();
-      return _userId;
-  }
-  module.exports.setRegardURL = function (url) {
-      _regardURL = url;
-  };
-  module.exports.setUserId = function (userId) {
-      _userId = userId;
-  };
-  module.exports.setSessionId = function (sessionId) {
-      _sessionId = sessionId;
-  };
-  module.exports.getUserId = function () {
-      return _userId
-  };
-  module.exports.getSessionId = function () {
-      return _sessionId;
-  };
+module.exports.trackEvent = _trackEvent;
+module.exports.setOrganization = function (org) {
+  _organization = org;
+};
+module.exports.setProduct = function (prod) {
+  _product = prod;
+};
