c23416ca5b15bfc71188f586b8fc8205224a0534
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-10 13:45:43 +0100

    More abusing of gulp. I should fix gulp-browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ var fs = require('fs');
 var gulp = require('gulp');
 var qunit = require('node-qunit-phantomjs');
 
+
+
 //var browserify = require('gulp-browserify');
 
 gulp.task('bundle', function() {
+  fs.mkdirSync(__dirname + '/dist/');;
+	
   var browserify = require('browserify');
 	browserify('./index.js')
 		.bundle({standalone: 'regardClient'})
